bc138c8e23cc4aaccf526edba4b95fee4122388c
vikass <vikas.srivastava@openet.com>
2022-11-23 14:53:28 +0000

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:mobODF1.iml

--- src/main/java/smallest_differenct/Program.java
+++ src/main/java/smallest_differenct/Program.java
@@ -4,13 +4,13 @@ public class Program {
 
     public static int[] smallestDifference(int[] arrayOne, int[] arrayTwo) {
         int [] result = new int[2];
-        int minimum_diff = 0;
+        int minimum_diff = Math.abs(arrayOne[0] - arrayTwo[0]);
         int minimum_diff_value_arrayOne = 0;
         int minimum_diff_value_arrayTwo = 0;
 
         for (int i = 0;i<arrayOne.length; i++) {
             for (int j = 0;j<arrayTwo.length;j++){
-
+                arrayOne[i] - arrayTwo[j];
             }
         }
 
