624083a204ca74c9ba7efc51f2029e9685559d83
Larissa Evaldt <lari.justo@gmail.com>
2022-11-23 15:00:26 +0000

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/main/java/smallest_differenct/Program.java

--- src/main/java/smallest_differenct/Program.java
+++ src/main/java/smallest_differenct/Program.java
@@ -1,17 +1,23 @@
 package smallest_differenct;
 
+import java.util.Arrays;
+
 public class Program {
 
     public static int[] smallestDifference(int[] arrayOne, int[] arrayTwo) {
         int [] result = new int[2];
         int minimum_diff = Math.abs(arrayOne[0] - arrayTwo[0]);
-        int minimum_diff_value_arrayOne = 0;
-        int minimum_diff_value_arrayTwo = 0;
-
+        int minimum_diff_value_arrayOne = arrayOne[0];
+        int minimum_diff_value_arrayTwo = arrayTwo[0];
+        Arrays.sort(arrayTwo);
         for (int i = 0;i<arrayOne.length; i++) {
             for (int j = 0;j<arrayTwo.length;j++){
-                arrayOne[i] - arrayTwo[j];
+                int temp = Math.abs(arrayOne[i] - arrayTwo[j]);
+                if (temp < minimum_diff) {
+
+                }
             }
+
         }
 
         return
