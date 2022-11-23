65a2ce9b92a8492bdf7596ece2181a3d583a049e
Aleksei Maurin <aleksei.maurin@openet.com>
2022-11-23 14:39:12 +0000

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/main/java/smallest_differenct/Program.java

---
+++ src/main/java/smallest_differenct/Program.java
@@ -0,0 +1,19 @@
+package smallest_differenct;
+
+public class Program {
+
+    public static int[] smallestDifference(int[] arrayOne, int[] arrayTwo) {
+        int [] result = new int[2];
+        int minimum_diff = 0;
+        int minimum_diff_value_arrayOne = 0;
+        int minimum_diff_value_arrayTwo = 0;
+
+        for (int i = 0;i<arrayOne.length; i++) {
+            for (int j = 0;j<arrayTwo.length;j++){
+
+            }
+        }
+
+        return
+    }
+}
