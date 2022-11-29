7b061cf5735ab50b5124e9fb60e6a016dea0966f
Aleksei Maurin <aleksei.maurin@openet.com>
2022-11-29 14:23:22 +0000

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/main/java/smallest_differenct/Program.java

--- src/main/java/smallest_differenct/Program.java
+++ src/main/java/smallest_differenct/Program.java
@@ -5,21 +5,30 @@ import java.util.Arrays;
 public class Program {
 
     public static int[] smallestDifference(int[] arrayOne, int[] arrayTwo) {
-        int [] result = new int[2];
+
+        // {-1, 5, 10, 20, 28, 3};
+        // {26, 134, 135, 15, 17};
         int minimum_diff = Math.abs(arrayOne[0] - arrayTwo[0]);
         int minimum_diff_value_arrayOne = arrayOne[0];
         int minimum_diff_value_arrayTwo = arrayTwo[0];
         Arrays.sort(arrayTwo);
-        for (int i = 0;i<arrayOne.length; i++) {
-            for (int j = 0;j<arrayTwo.length;j++){
+        // {15, 17, 26, 134, 135};
+        for (int i = 0; i < arrayOne.length; i++) {
+            for (int j = 0; j < arrayTwo.length; j++) {
                 int temp = Math.abs(arrayOne[i] - arrayTwo[j]);
                 if (temp < minimum_diff) {
-
+                    minimum_diff = temp;
+                    minimum_diff_value_arrayOne = arrayOne[i];
+                    minimum_diff_value_arrayTwo = arrayTwo[j];
+                } else {
+                    break;
                 }
             }
 
         }
 
-        return null;
+        int[] result = {minimum_diff_value_arrayOne, minimum_diff_value_arrayTwo};
+
+        return result;
     }
 }
