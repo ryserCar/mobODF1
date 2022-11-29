fc33005d8e0956f6b3a9fcbb755499666f6f5487
Aleksei Maurin <aleksei.maurin@openet.com>
2022-11-29 14:45:25 +0000

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/main/java/smallest_differenct/Program.java

--- src/main/java/smallest_differenct/Program.java
+++ src/main/java/smallest_differenct/Program.java
@@ -13,21 +13,17 @@ public class Program {
         int minimum_diff_value_arrayTwo = arrayTwo[0];
         Arrays.sort(arrayTwo);
         // {15, 17, 26, 134, 135};
-        outerLopp:
+
         for (int i = 0; i < arrayOne.length; i++) {
             for (int j = 0; j < arrayTwo.length; j++) {
                 int temp = Math.abs(arrayOne[i] - arrayTwo[j]);
-                if (temp < minimum_diff) {
-                    minimum_diff = temp;
-                    minimum_diff_value_arrayOne = arrayOne[i];
-                    minimum_diff_value_arrayTwo = arrayTwo[j];
-                } else {
-                    break outerLopp;
-                }
+                if (temp >= minimum_diff) continue;
+                System.out.println(i + " : " + j);
+                minimum_diff = temp;
+                minimum_diff_value_arrayOne = arrayOne[i];
+                minimum_diff_value_arrayTwo = arrayTwo[j];
             }
-
         }
-
         int[] result = {minimum_diff_value_arrayOne, minimum_diff_value_arrayTwo};
         System.out.println(Arrays.toString(result));
         return result;
