4b6c954ccaae593277fe31687d651ba9ad8c56f2
Larissa Evaldt <lari.justo@gmail.com>
2022-11-29 14:10:19 +0000

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test/java/smallest_differenct/ProgramTest.java

---
+++ src/test/java/smallest_differenct/ProgramTest.java
@@ -0,0 +1,16 @@
+package smallest_differenct;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProgramTest {
+
+    @Test
+    void name() {
+        int[] arrayOne = {-1, 5, 10, 20, 28, 3};
+        int[] arrayTwo = {26, 134, 135, 15, 17};
+
+        int[] output = {28, 26};
+    }
+}
