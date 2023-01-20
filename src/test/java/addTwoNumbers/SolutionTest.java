ff1076c867f3aede71e2c81b463bf00af17b7568
Larissa Evaldt <lari.justo@gmail.com>
2023-01-20 12:17:26 +0000

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test/java/addTwoNumbers/SolutionTest.java

--- src/test/java/addTwoNumbers/SolutionTest.java
+++ src/test/java/addTwoNumbers/SolutionTest.java
@@ -1,13 +1,11 @@
 package addTwoNumbers;
 
-import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Test;
 
-import javax.xml.transform.SourceLocator;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
 
@@ -16,6 +14,23 @@ class SolutionTest {
     public void shouldReturnEightZeroSeven() {
         List list1 = Arrays.asList(2, 4, 3);
         List list2 = Arrays.asList(5, 6, 4);
-        assertTrue(testClass.addTwoNumbers(list1,list2);
+        List expected = Arrays.asList(7, 0, 8);
+        assertEquals(expected, testClass.addTwoNumbers(list1,list2));
+    }
+
+    @Test
+    public void testTwoArraysOfDifferentSizes() {
+        List list1 = Arrays.asList(9, 9, 9, 9, 9, 9, 9);
+        List list2 = Arrays.asList(9, 9, 9, 9);
+        List expected = Arrays.asList(8, 9, 9, 9, 0, 0, 0, 1);
+        assertEquals(expected, testClass.addTwoNumbers(list1,list2));
+    }
+
+    @Test
+    public void testArraysWithValueZero() {
+        List list1 = Arrays.asList(0);
+        List list2 = Arrays.asList(0);
+        List expected = Arrays.asList(0);
+        assertEquals(expected, testClass.addTwoNumbers(list1,list2));
     }
 }
