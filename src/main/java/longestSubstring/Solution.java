2a7d919baaee5a36e0ccb438ca48cceedc5f11a0
vikass <vikas.srivastava@openet.com>
2023-01-25 15:40:21 +0000

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/main/java/longestSubstring/Solution.java

--- src/main/java/longestSubstring/Solution.java
+++ src/main/java/longestSubstring/Solution.java
@@ -7,18 +7,37 @@ public class Solution {
 
     public int lengthOfLongestSubstring(String s) {
         int result = 0;
-        Set<Character> charSet = new HashSet<Character>();
-        char[] characters = s.toCharArray();
-        int count = 0;
-        for (int i = 0; i < characters.length; i++) {
-            char a = characters[i];
-            //abcabcbb  abcabcde  anviaj
-            // if charSet.get(character) ;
-            // if not presented we add to charSet and increase count
-            // if presented we clean charSet, set result equals to count if count > result. result = 3 if counter > result.
+        int size = s.length();
+        for (int i = 0; i < size; i++) {
+            for (int j = i; j < size; j++){
+                if (isLongestSubStringWithoutRepeatingCharacter(s, i, j)) {
+                     // increase the count;
+                }
+            }
+
 
         }
 
         return 0;
+    }
+
+    private boolean isLongestSubStringWithoutRepeatingCharacter(String str, int start, int end){
+        Set<Character> mySet = new HashSet<>();
+        for (int k = 0; k <= end; k++){
+            Character c = str.charAt(k);
+            if (mySet.contains(c)) {
+                return false;
+            }
+            mySet.add(c);
+        }
+        return true;
+
+
+        // char a = characters[i];
+        //abcabcbb  abcabcde  anviaj
+        // if charSet.get(character) ;
+        // if not presented we add to charSet and increase count
+        // if presented we clean charSet, set result equals to count if count > result. result = 3 if counter > result.
+        return false;
     }
 }
