20d41592cc222a585ded51d01a48cb4b77975ebb
Aleksei Maurin <aleksei.maurin@openet.com>
2023-01-25 15:30:24 +0000

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/main/java/longestSubstring/Solution.java

--- src/main/java/longestSubstring/Solution.java
+++ src/main/java/longestSubstring/Solution.java
@@ -1,11 +1,23 @@
 package longestSubstring;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
 
     public int lengthOfLongestSubstring(String s) {
         int result = 0;
-
+        Set<Character> charSet = new HashSet<Character>();
         char[] characters = s.toCharArray();
+        int count = 0;
+        for (int i = 0; i < characters.length; i++) {
+            char a = characters[i];
+            //abcabcbb  abcabcde  anviaj
+            // if charSet.get(character) ;
+            // if not presented we add to charSet and increase count
+            // if presented we clean charSet, set result equals to count if count > result. result = 3 if counter > result.
+
+        }
 
         return 0;
     }
